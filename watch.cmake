c3f88c1090e9e78d1985f9a713ddc76fd4b19adf
honeyspoon <bobmatt911@gmail.com>
2024-05-20 16:27:57 -0400

    fix watch

--- watch.cmake
+++ watch.cmake
@@ -13,10 +13,11 @@ function(watch_files)
     execute_process(COMMAND fswatch -1 ${CMAKE_CURRENT_SOURCE_DIR}
                     OUTPUT_VARIABLE FILE_CHANGED)
 
+    message("File changed: ${FILE_CHANGED}")
     if(FILE_CHANGED)
-      string(FIND "${FILE_CHANGED}" ".cpp" CPP_POS)
-      string(FIND "${FILE_CHANGED}" ".h" H_POS)
-      string(FIND "${FILE_CHANGED}" "CMakeLists.txt" CMAKE_POS)
+      string(FIND "${FILE_CHANGED}" ".cpp$" CPP_POS)
+      string(FIND "${FILE_CHANGED}" ".h$" H_POS)
+      string(FIND "${FILE_CHANGED}" "CMakeLists.txt$" CMAKE_POS)
 
       if(CPP_POS GREATER -1
          OR H_POS GREATER -1
